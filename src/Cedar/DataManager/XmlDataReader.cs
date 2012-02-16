dc7383841c74f95e38c766e3663793a1ad0ae3c2
Tarun Kumar <tarun.kumar.singh@gmail.com>
2012-02-16 13:57:22 +0530

    Implemented IDataReader interface

--- src/Cedar/DataManager/XmlDataReader.cs
+++ src/Cedar/DataManager/XmlDataReader.cs
@@ -7,6 +7,19 @@ namespace Cedar.DataManager
 {
     class XmlDataReader : IDataReader
     {
+        public List<Shard> GetAllShardByAppname(string appName)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Shard GetShardById(long shardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Shard> GetAllShard()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
