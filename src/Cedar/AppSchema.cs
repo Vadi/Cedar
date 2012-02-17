bb0f57bd678494d37839c992a89468541287fda5
Tarun Kumar <tarun.kumar.singh@gmail.com>
2012-02-17 16:40:50 +0530

    Add the class the store application schema entity

---
+++ src/Cedar/AppSchema.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cedar
+{
+    public class AppSchema
+    {
+        public int application_id { get; set; }
+        public string application_name { get; set; }
+        public string schema { get; set; }
+    }
+}
