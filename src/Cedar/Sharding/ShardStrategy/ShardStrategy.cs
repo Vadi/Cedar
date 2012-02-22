39e15ca47e640631e071e1db588bc067c1eb46bf
Tarun Kumar <tarun.kumar.singh@gmail.com>
2012-02-22 18:43:56 +0530

    Added new classes for Shard strategy

---
+++ src/Cedar/Sharding/ShardStrategy/ShardStrategy.cs
@@ -0,0 +1,26 @@
+﻿using Cedar.Sharding.ShardStrategy.ShardAccess;
+using Cedar.Sharding.ShardStrategy.ShardResolution;
+using Cedar.Sharding.ShardStrategy.ShardSelection;
+
+namespace Cedar.Sharding.ShardStrategy
+{
+    public class ShardStrategy<T>:IShardStrategy<T>
+    {
+
+        public IShardSelectionStrategy<T> ShardSelectionStrategy
+        {
+            get { return new ShardSelectionStrategy<T>(); }
+        }
+
+        public IShardResolutionStrategy ShardResolutionStrategy
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public IShardAccessStrategy ShardAccessStrategy
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+    }
+    
+}
