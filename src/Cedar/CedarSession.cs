7e24842eeb5b18d5f5a718bbcf8471c623ba2400
Tarun Kumar <tarun.kumar.singh@gmail.com>
2012-02-22 17:26:27 +0530

    Made changes in get connection string

--- src/Cedar/CedarSession.cs
+++ src/Cedar/CedarSession.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using Cedar.Balancer;
@@ -12,46 +13,47 @@ using System.Data.OleDb;
 using Dapper;
 namespace Cedar
 {
-    public class CedarSession: ICedarSession
+    public class CedarSession : ICedarSession
     {
         private IDbConnection _sqlConnection = null;
         private long _uuid;
         String _connectionString = String.Empty;
         public bool disposed { get; set; }
         private int _commandTimeout = 30;
-        private System.Data.CommandType? _commandType = null; 
+        private System.Data.CommandType? _commandType = null;
         public int CommandTimeout { get { return _commandTimeout; } set { _commandTimeout = value; } }
-        private IDbTransaction _transaction=null ;
+        private IDbTransaction _transaction = null;
         public bool EnableTrasaction { get; set; }
         public CedarSession(long uuid)
         {
             _uuid = uuid;
-            setConnectionString();
+            SetConnectionString();
             GetOpenConnection();
-            
+
         }
+
+
         /// <summary>
         /// Dispose the object
         /// </summary>
         public void Close()
         {
-           Dispose();
+            Dispose();
         }
-        private void setConnectionString()
+        private void SetConnectionString()
         {
-            var reader = new Cedar.DataManager.SqlDataReader();
+            var reader = new Cedar.SqlDataReader();
+            IdWorker worker=new IdWorker(_uuid);
+            //worker.DecomposeKey()
             var shard = reader.GetShardById(_uuid);
-            _connectionString = shard.connection_string  ;
+            _connectionString = shard.connection_string;
         }
-        public AppContext GetAppContext()
-        {
-            throw new NotImplementedException();
-        }
-        internal void SetupSchema(App app,AppSchema appSchema)
+       
+        internal void SetupSchema(App app, AppSchema appSchema)
         {
             _commandType = System.Data.CommandType.Text;
             _transaction = _sqlConnection.BeginTransaction();
-            SqlMapper.Execute(_sqlConnection, appSchema.schema , app.AppId, _transaction, _commandTimeout, _commandType);
+            SqlMapper.Execute(_sqlConnection, appSchema.schema, app.AppId, _transaction, _commandTimeout, _commandType);
 
         }
         /// <summary>
@@ -61,13 +63,13 @@ namespace Cedar
         /// <param name="param"></param>
         /// <param name="commandType"></param>
         /// <returns>>Number of rows affected</returns>
-        public int Insert(string sql, dynamic param = null, Cedar.CommandType ? commandType = null)
+        public int Insert(string sql, dynamic param = null, Cedar.CommandType? commandType = null)
         {
-            if (commandType!=null )
+            if (commandType != null)
                 _commandType = System.Data.CommandType.Text;
             if (EnableTrasaction)
                 _transaction = _sqlConnection.BeginTransaction();
-          return SqlMapper.Execute(_sqlConnection,sql,param ,_transaction ,_commandTimeout ,_commandType);
+            return SqlMapper.Execute(_sqlConnection, sql, param, _transaction, _commandTimeout, _commandType);
         }
 
         public void Update(string sql, dynamic param = null, Cedar.CommandType? commandType = null)
@@ -96,7 +98,7 @@ namespace Cedar
                 _transaction = _sqlConnection.BeginTransaction();
             return SqlMapper.Query<dynamic>(_sqlConnection, sql, param, _transaction, true, _commandTimeout, _commandType);
         }
-      
+
         public void Dispose()
         {
             Dispose(true);
@@ -118,7 +120,7 @@ namespace Cedar
                         _sqlConnection = null;
                     }
                 }
-               
+
                 disposed = true;
             }
         }
@@ -129,38 +131,53 @@ namespace Cedar
 
         private void GetOpenConnection()
         {
-            if(_sqlConnection ==null && !String.IsNullOrEmpty(_connectionString))
+            if (_sqlConnection == null && !String.IsNullOrEmpty(_connectionString))
             {
-                string provider = GetProvider(_connectionString);
-                switch(provider)
-                {
-                    case "SQL":
-                        _sqlConnection = new SqlConnection(_connectionString);
-                       break;
-                    case "ORACLE":
-                        _sqlConnection = new OracleConnection(_connectionString);
-                        break;
-                    case "ODBC":
-                        _sqlConnection = new OdbcConnection(_connectionString);
-                        break;
-                    case "OLEDB":
-                        _sqlConnection = new OleDbConnection(_connectionString);
-                        break;
-                }
-                
-                _sqlConnection.Open();
-               
+                _sqlConnection = GetConnection(_connectionString);
+               if(_sqlConnection!=null )
+               {
+                   _sqlConnection.ConnectionString = _connectionString;
+                   _sqlConnection.Open();   
+               }
+               else
+               {
+                   throw new Exception("Connection string can not be null, you may not specified the provider name");
+               }
             }
 
         }
-        public string GetProvider(string connectionString)
+        private DbConnection GetConnection(string connectionString)
         {
-            string  provider = "SQL";            
-           
-            return provider;
+            string providerName=String.Empty ;
+
+
+            //try to build connection string for sql
+            var builder = new DbConnectionStringBuilder();
+            builder.ConnectionString = connectionString;
+
+            if (builder.ContainsKey("provider"))
+            {
+                providerName = builder["provider"].ToString();
+            }
+            if (String.IsNullOrEmpty(providerName))
+            {
+                providerName = "System.Data.SqlClient";
+            }
+            var providerExists = DbProviderFactories
+                                            .GetFactoryClasses()
+                                            .Rows.Cast<DataRow>()
+                                            .Any(r => r[2].Equals(providerName));
+                if (providerExists)
+                {
+                    var factory = DbProviderFactories.GetFactory(providerName);
+                    return factory.CreateConnection();
+                }
+            
+            
+            return null;
         }
 
-     
+
     }
 
     public enum CommandType
@@ -168,5 +185,5 @@ namespace Cedar
         Query,
         StoredProcedure
     }
-    
+
 }
