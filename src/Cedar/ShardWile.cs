2e4fa651fb99328e3df252f7c774574af9331954
Tarun Kumar <tarun.kumar.singh@gmail.com>
2012-02-22 17:45:16 +0530

    Add the class to map the table ShardWile

---
+++ src/Cedar/ShardWile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cedar
+{
+    public class ShardWile
+    {
+        public int Id { get; set; }
+        public int ShardId { get; set; }
+        public int TotalCount { get; set; }
+        public int MaxCount { get; set; }
+    }
+}
