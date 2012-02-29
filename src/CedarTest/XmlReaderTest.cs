fb25c32337e8b4ad743f144e3fc61fa44ddbdc0d
Hemant Roy <hemant@hcitec.com>
2012-02-29 16:06:29 +0530

    added test case

---
+++ src/CedarTest/XmlReaderTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cedar;
+namespace CedarTest
+{
+    [TestClass]
+    public class XmlReaderTest
+    {
+        [TestMethod]
+        public void GetShardById()
+        {
+            IDataReader dataReader = new DataFactory().GetdataReader(FetchType.Xml);
+            Shard shard = dataReader.GetShardById(0);
+            Assert.IsTrue(shard != null );           
+        }
+
+        [TestMethod]
+        public void GetAllShard()
+        {
+            IDataReader dataReader = new DataFactory().GetdataReader(FetchType.Xml);
+            List<Shard> shardList  = dataReader.GetAllShard();
+            Assert.IsTrue(shardList.Count > 0 );
+        }
+
+
+        [TestMethod]
+        public void GetAllShardByAppname()
+        {
+            IDataReader dataReader = new DataFactory().GetdataReader(FetchType.Xml);
+            List<Shard> shardList = dataReader.GetAllShardByAppname("CxC");
+            Assert.IsTrue(shardList.Count > 0);
+        }
+
+        [TestMethod]
+        public void GetShardStrategyById()
+        {
+            IDataReader dataReader = new DataFactory().GetdataReader(FetchType.Xml);
+            IList<ShardWile> shardWileList = dataReader.GetShardStrategyById(0);
+            Assert.IsTrue(shardWileList != null);
+        }
+        
+    }
+
+
+   
+
+
+}
