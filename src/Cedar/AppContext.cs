ead36b631c131ee976021d3216756577cabd99dc
Hemant Roy <hemant@hcitec.com>
2012-02-29 16:47:28 +0530

    Merged the code

--- src/Cedar/AppContext.cs
+++ src/Cedar/AppContext.cs
@@ -14,11 +14,11 @@ namespace Cedar
     public class AppContext
     {
         private App _app;
-      
+
         // 1. I need to have all shards that are configured for this app
-        // 2. I need to know how to decode the uuid and understand a shard id out of it 
+        // 2. I need to know how to decode the uuid and understand a shard id out of it
         // 3. I need to know a shard resolution strategy
-        public AppContext(string appName) 
+        public AppContext(string appName)
         {
             _app = new App();
             _app.ApplicationName = appName;
@@ -35,23 +35,27 @@ namespace Cedar
             // call cedar data method which would execute schema def statements in above shard id
 
             var dataReader = new DataFactory().GetdataReader(FetchType.Sql);
-            var shards= dataReader.GetAllShardByAppname(_app.ApplicationName);
+            var shards = dataReader.GetAllShardByAppname(_app.ApplicationName);
             _app.Shards = shards;
             IShardStrategy<App> shardStrategy = new ShardStrategy<App>();
-            var shardSelection=shardStrategy.ShardSelectionStrategy;
-            var shardId=shardSelection.SelectShardIdForExistingObject(_app);
-            IdWorker worker = new IdWorker(shardId);
-            var UniqueId = worker.GetUniqueId();
-            dataReader.SetupSchema(shardId, UniqueId);
-           
-            return UniqueId;
+            var shardSelection = shardStrategy.ShardSelectionStrategy;
+            var shardId = shardSelection.SelectShardIdForExistingObject(_app);
+            var worker = new IdWorker(shardId);
+            var uniqueId = worker.GetUniqueId();
+            var appSchema = dataReader.GetAppSchema(shardId);
+            var cedarSession = new CedarSession(uniqueId);
+            cedarSession.EnableTrasaction = true;
+            cedarSession.SetupSchema(appSchema);
+            cedarSession.Close();
+            dataReader.UpdateShardWile(shardId);
+            return uniqueId;
 
         }
 
         public ICedarSession GetSession(long uuid)
         {
             return new CedarSession(uuid);
-            
+
         }
 
     }
