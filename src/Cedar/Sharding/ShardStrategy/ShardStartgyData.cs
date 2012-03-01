0d771c67f5bd2170e7447320b04f141375fe61fd
Prakashb <prakash@hcitec.com>
2012-03-01 17:19:15 +0530

    adding more files

---
+++ src/Cedar/Sharding/ShardStrategy/ShardStartgyData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cedar.Sharding.ShardStrategy;
+
+namespace Cedar
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ShardStartegyData
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Strategy StrategyType { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Region { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        internal App App { get; set; }
+    }
+}
